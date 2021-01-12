61d97644197fccbb488c0913f86dd994bd2cdd37
kuniwyl <wylukkozak1@gmail.com>
2021-01-12 17:43:13 +0100

    Dodanie eliminacji

--- trunk/src/gauss.c
+++ trunk/src/gauss.c
@@ -5,10 +5,46 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
-
-		return 0;
+        int r = mat->r;
+        int c = mat->c;
+	int i;
+	int j;
+	int k;
+	
+	for( i=0; i<r-1; i++)
+	{
+	for( j=i+1; j<r; j++)
+	{
+		double max= mod(mat->data[i][i]);
+		int y=i+1;
+		int ya=i;
+		for(; y<r;y++)
+		{
+			if(mod(mat->data[y][i]) > max)
+			{
+				ya = y;
+				max = mod(mat->data[y][i]);
+			}
+		}
+		k=0;
+		for(; k < c; k++)
+		{
+			double tym;
+			tym = mat->data[i][k];
+			mat->data[i][k] = mat->data[ya][k];
+			mat->data[ya][k] = tym;
+		}
+		double z = b->data[i][0];
+		b->data[i][0] = b->data[ya][0];
+		b->data[ya][0] = z;
+		double tym = (-1) * mat->data[j][i] / mat->data[i][i];	
+		for( k=i; k<r; k++)
+		{
+			mat-> data[j][k] += tym * mat->data[i][k]; 
+		}
+		b->data[j][0] += tym * b->data[i][0];
+	}
+}
+	return 0;
 }
 
